f9c38558f16e2fb6fddd256a2fb8dc02df71f420
AlexUrluescu <urluescualex2003@gmail.com>
2023-06-25 16:14:48 +0300

    py_manage: Create the connection to the endpoint

---
+++ py_manage/test.py
@@ -0,0 +1,27 @@
+import requests
+
+# Funcția pentru a trimite datele către API în cloud
+def send_sensor_data_to_cloud(data):
+    url = 'http://localhost:5000/datasenzor'  # Inlocuiți cu URL-ul real al API-ului în cloud
+
+    # Trimiteți datele prin cererea POST către API-ul în cloud
+    response = requests.post(url, json=data)
+
+    # Verificați răspunsul de la API
+    if response.status_code == 200:
+        print('Datele au fost trimise cu succes către API în cloud.')
+
+        data = response.json()
+        print(data)
+    else:
+        print('Eroare la trimiterea datelor către API în cloud.')
+
+# Exemplu de date de la senzori
+sensor_data = {
+    'temperature': 25.5,
+    'humidity': 60.2,
+    # alte date de la senzori
+}
+
+# Apelați funcția pentru a trimite datele către API în cloud
+send_sensor_data_to_cloud(sensor_data)
