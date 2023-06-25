7d7b0a3124ea597e201793aa18416c9bad2c0a7b
AlexUrluescu <urluescualex2003@gmail.com>
2023-06-25 12:23:44 +0300

    py_manage: Insert document with senzor class date

--- py_manage/py_data.py
+++ py_manage/py_data.py
@@ -1,5 +1,9 @@
 import pymongo
 
+from senzor import dht_sensor
+
+senzor = dht_sensor()
+
 conn = "mongodb+srv://flaviu:parola@cluster0.kzya4z8.mongodb.net/?retryWrites=true&w=majority"
 
 try:
@@ -18,6 +22,22 @@ try:
     print(data["temperature"])
     print(data["humidity"])
     print(data["date"])
+
+    temp = senzor.get_t()
+    hum = senzor.get_h()
+
+    print(temp)
+    print(hum)
+
+    doc = {
+        "temperature": temp,
+        "humidity": hum,
+        "date": "2023-06-25",
+    }
+
+    rezultat = colection.insert_one(doc)
+
+    print("ID-ul documentului inserat:", rezultat.inserted_id)
 
     client.close()
 
